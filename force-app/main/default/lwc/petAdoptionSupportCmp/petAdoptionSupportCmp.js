4979968193d605f6cf8f5e1599e585f4240a2987
Mikolaj <salekmikolaj2002@gmail.com>
2025-08-19 15:13:39 +0200

    lighning button problem

--- force-app/main/default/lwc/petAdoptionSupportCmp/petAdoptionSupportCmp.js
+++ force-app/main/default/lwc/petAdoptionSupportCmp/petAdoptionSupportCmp.js
@@ -1,38 +1,34 @@
-import { LightningElement} from 'lwc';
+import { LightningElement } from 'lwc';
 import getAllPets from '@salesforce/apex/AdoptionHandler.getAllPets';
 import getAdoptedPets from '@salesforce/apex/AdoptionHandler.getAdoptedPets';
 import getNotAdoptedPets from '@salesforce/apex/AdoptionHandler.getNotAdoptedPets';
 
 export default class PetAdoptionSupportCmp extends LightningElement {
     result;
+    error;
 
-    queryall() {
-        getAllPets()
+    queryPets(event) {
+        const query_type = event.target.dataset.querytype;
+        let promise;
+
+        switch (query_type) {
+            case 'all':
+                promise = getAllPets();
+            case 'adopted':
+                promise = getAdoptedPets();
+            case 'notadopted':
+                promise = getNotAdoptedPets();
+        }
+        
+        promise
             .then((result) => {
                 this.result = result;
-                console.log(this.result.data);
+                this.error = null;
+                console.log(result);
             })
             .catch((error) => {
-                console.error(error);
-            });
-    }
-
-    queryadopted() {
-        getAdoptedPets()
-            .then((result) => {
-                this.result = result;
-            })
-            .catch((error) => {
-                console.error(error);
-            });
-    }
-
-    querynotadopted() {
-        getNotAdoptedPets()
-            .then((result) => {
-                this.result = result;
-            })
-            .catch((error) => {
+                this.result = null;
+                this.error = error;
                 console.error(error);
             });
     }
